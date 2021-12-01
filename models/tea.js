7b6f311d8451ac9bbc164d1414f92dad4cc93349
AlekhyaTumee <89401028+AlekhyaTumee@users.noreply.github.com>
2021-11-30 23:00:25 -0600

    added protection for endpoints and validators for data

--- models/tea.js
+++ models/tea.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose") 
-const teaSchema = mongoose.Schema({ tea_type: {type: String,minlength: 3}, flavour: String, tea_price: Number }) 
+const teaSchema = mongoose.Schema({ tea_type: {type: String,minlength: 3}, flavour: String, tea_price: {type: Number,min: 1.4 }}) 
  
 module.exports = mongoose.model("tea", teaSchema) 
